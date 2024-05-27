8e8d72d378881a2c96e79cac50cbd2d0b35d424e
Tank <944627108@qq.com>
2024-05-27 10:41:55 +0800

    图片处理

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <main class="wrapper">
       <div id="shopify-section-home" class="shopify-section">
         <div class="homepage">
-          <router-view />
+          <!-- <router-view /> -->
         </div>
       </div>
     </main>
@@ -13,36 +13,33 @@
   </nav>
 </template>
 <script>
-  import { defineComponent, getCurrentInstance, nextTick, onMounted, reactive, toRefs } from "vue";
-  import Header from "@/layout/Header.vue";
-  import Footer from "@/layout/Footer.vue";
-  import Minors from "@/layout/Minors.vue";
-  import "@/assets/style/home.less"
-  export default defineComponent({
-    components: {
-      Header,
-      Footer,
-      Minors,
-    },
-    props: {
-      modalParam: {
-        type: Object,
-        default: () => { return {} },
-      },
-    },
-    setup() {
-      const { proxy } = getCurrentInstance();
-      const state = reactive({
-      })
-      onMounted(async () => {
-      
-      })
-      return {
-        ...toRefs(state),
+import { defineComponent, getCurrentInstance, nextTick, onMounted, reactive, toRefs } from 'vue'
+import Header from '@/layout/Header.vue'
+import Footer from '@/layout/Footer.vue'
+import Minors from '@/layout/Minors.vue'
+import '@/assets/style/home.less'
+export default defineComponent({
+  components: {
+    Header,
+    Footer,
+    Minors
+  },
+  props: {
+    modalParam: {
+      type: Object,
+      default: () => {
+        return {}
       }
     }
-  });
+  },
+  setup() {
+    const { proxy } = getCurrentInstance()
+    const state = reactive({})
+    onMounted(async () => {})
+    return {
+      ...toRefs(state)
+    }
+  }
+})
 </script>
-<style lang="less">
-
-</style>
+<style lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,47 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
-    path: "/",
-    name: "/",
-    component: () =>
-      import("@/views/home/index.vue"),
+    path: '/',
+    name: '/',
+    component: () => import('@/views/home/index.vue')
   },
   {
-    path: "/home",
-    name: "home",
-    title: 'EHONOS',
-    component: () =>
-      import("@/views/home/index.vue"),
+    path: '/home',
+    name: 'home',
+    title: '造光',
+    component: () => import('@/views/home/index.vue')
   },
   {
-    path: "/products",
-    name: "products",
-    component: () => import("@/views/products/index.vue"),
+    path: '/products',
+    name: 'products',
+    component: () => import('@/views/products/index.vue')
   },
   {
-    path: "/productsDetail",
-    name: "productsDetail",
-    component: () => import('@/views/productsDetail/index.vue'),
+    path: '/productsDetail',
+    name: 'productsDetail',
+    component: () => import('@/views/productsDetail/index.vue')
   },
   {
-    path: "/wholesale",
-    name: "wholesale",
-    component: () =>
-      import("@/views/wholesale/index.vue"),
+    path: '/wholesale',
+    name: 'wholesale',
+    component: () => import('@/views/wholesale/index.vue')
   },
   {
-    path: "/contact",
-    name: "contact",
-    component: () =>
-      import("@/views/contact/index.vue"),
+    path: '/contact',
+    name: 'contact',
+    component: () => import('@/views/contact/index.vue')
   },
   {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import("@/views/about/index.vue"),
-  },
-];
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/about/index.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
